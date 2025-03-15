6680cc9f85ec603677fd197d277a6b70f6239ac8
Matthew Bautista <matthjb2@uci.edu>
2025-03-14 19:27:52 -0700

    Ladder print fix.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -69,10 +69,8 @@ void load_words(set<string> &word_list, const string &file_name) {
 void print_word_ladder(const vector<string> &ladder) {
     if (ladder.empty()) cout << "No word ladder found." << endl;
     else {
-        for (size_t i = 0; i < ladder.size(); ++i) {
-            if (i > 0) cout << " -> ";
-            cout << ladder[i];
-        }
+        cout << "Word ladder found: ";
+        for (size_t i = 0; i < ladder.size(); ++i) cout << ladder[i] << " ";
         cout << endl;
     }
 }
