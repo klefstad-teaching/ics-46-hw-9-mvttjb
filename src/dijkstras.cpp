8aac883b18ca7b1987c16fc85c167c4963fe9c76
Matthew Bautista <matthjb2@uci.edu>
2025-03-14 19:36:29 -0700

    uh will there always be a path..

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -54,10 +54,6 @@ vector<int> extract_shortest_path(const vector<int> &distances, const vector<int
 }
 
 void print_path(const vector<int>& path, int total) {
-    if (path.empty()) {
-        return;
-    } else {
-        for (size_t i = 0; i < path.size(); ++i) cout << path[i] << " ";
-        cout << endl << "Total cost is " << total << endl;
-    }
+    for (size_t i = 0; i < path.size(); ++i) cout << path[i] << " ";
+    cout << endl << "Total cost is " << total << endl;
 }
